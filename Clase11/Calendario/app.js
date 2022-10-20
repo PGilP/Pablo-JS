fca6c651ba0109d5c99e75a9baccf82f1300df2f
Pablo <pgilp17@protonmail.com>
2022-10-20 11:57:52 +0200

    Calendario

--- Clase11/Calendario/app.js
+++ Clase11/Calendario/app.js
@@ -1,37 +1,62 @@
 'use strict'
 
 let nodoAnio = document.querySelector( '.anio' );
-let nodoPrimerMes = document.querySelector( '.mes .numeroDia' );
+let nodoMeses = document.querySelectorAll( '.mes .numeroDia' );
 
 let today;
-let anioActual;
-
-let diasHTML = '';
-let diasMes = 28;
-
+let anioActual = 2022;
 
 
 conseguirDiaActual();
-rellenarMes();
+rellenarCalendario(anioActual);
 
 
-function rellenarMes(){
+function rellenarCalendario(anio){
+    rellenarMeses(anio);
     
-    for (let i = 1; i < diasMes+1; i++) {
-        if(i === 1){
-            diasHTML += `<div class="primerDia">${i}</div>`;
-        }else{
-            diasHTML += `<div>${i}</div>`;
+    function rellenarMeses(anio){
+        
+        for (let i = 0; i < 2; i++) {
+            console.log('entro en el for')
+            console.log(anio,i);
+    
+            //Creando esta fecha se accede al primer dia del mes
+            console.log(new Date(anio,i));
+            let primerDia = (new Date(anio,i)).getDay();
+            console.log(primerDia);
+            
+
+            console.log('mes-->',new Date(anio, i, 0))
+            console.log('numeroDias-->',new Date(anio, i, 0).getDate());
+    
+            let contenidoDinamico = rellenarMes(new Date(anio, i, 0).getDate(),primerDia);
+            nodoMeses[i].innerHTML = contenidoDinamico;
+    
         }
     }
 
-    nodoPrimerMes.innerHTML = diasHTML;
-
+    function rellenarMes(diasMes,primerDia){
+        console.log('primerdia--->',primerDia);
+        let diasHTML = '';
+        for (let i = 0; i < diasMes; i++) {
+            if(i === 0){
+                diasHTML += `<div class="primerDia" style="grid-column-start: ${primerDia};"> <time datetime="">${i+1}</div>`;
+            }else{
+                diasHTML += `<div>${i+1}</div>`;
+            }
+        }
+        return diasHTML;
+    }
 }
+
+
+
+
+
 
 function conseguirDiaActual(){
     today = new Date();
-    console.log((today.getFullYear(), today.getMonth() + 1, 0).getDate());
+    
     pintarAnio(today.getFullYear());
 }
 

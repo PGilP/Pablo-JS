fa1dead7652e7561704437674b60bcb26038e1b8
Pablo <pgilp17@protonmail.com>
2022-10-21 10:50:39 +0200

    Calendario Teminado

--- Clase11/Calendario/app.js
+++ Clase11/Calendario/app.js
@@ -1,65 +1,61 @@
 'use strict'
 
-let nodoAnio = document.querySelector( '.anio' );
-let nodoMeses = document.querySelectorAll( '.mes .numeroDia' );
+let nodoAnio      = document.querySelector( '.anio' );
+let nodoMeses     = document.querySelectorAll( '.mes .numeroDia' );
+let nodoSiguiente = document.querySelector( '#siguiente' );
+let nodoAnterior  = document.querySelector( '#anterior' );
 
-let today;
-let anioActual = 2022;
+let today       = new Date();
+let anioActual  = new Date().getFullYear();
+const NUM_MESES = 12;
 
-
-conseguirDiaActual();
+nodoAnio.innerText = anioActual;
 rellenarCalendario(anioActual);
 
+nodoAnterior.addEventListener( 'click' , function(){
+    anioActual -= 1;
+    nodoAnio.innerText = anioActual;
+    rellenarCalendario(anioActual);
+});
+
+nodoSiguiente.addEventListener( 'click' , function(){
+    anioActual += 1;
+    nodoAnio.innerText = anioActual;
+    rellenarCalendario(anioActual);
+});
 
 function rellenarCalendario(anio){
     rellenarMeses(anio);
     
     function rellenarMeses(anio){
         
-        for (let i = 0; i < 2; i++) {
-            console.log('entro en el for')
-            console.log(anio,i);
-    
-            //Creando esta fecha se accede al primer dia del mes
-            console.log(new Date(anio,i));
-            let primerDia = (new Date(anio,i)).getDay();
-            console.log(primerDia);
-            
+        for (let i = 0; i < NUM_MESES; i++) {
+            let mes = new Date(anio, i);
 
-            console.log('mes-->',new Date(anio, i, 0))
-            console.log('numeroDias-->',new Date(anio, i, 0).getDate());
+            //Creando esta fecha se accede al primer dia del mes
+            let primerDia  = mes.getDay();
+            let numeroDias = new Date(mes.getFullYear(),mes.getMonth()+1,0).getDate();
     
-            let contenidoDinamico = rellenarMes(new Date(anio, i, 0).getDate(),primerDia);
+            let contenidoDinamico = rellenarMes(numeroDias,primerDia);
             nodoMeses[i].innerHTML = contenidoDinamico;
-    
+
         }
     }
 
     function rellenarMes(diasMes,primerDia){
-        console.log('primerdia--->',primerDia);
+        console.log('PrimerDia-->',primerDia);
         let diasHTML = '';
         for (let i = 0; i < diasMes; i++) {
             if(i === 0){
-                diasHTML += `<div class="primerDia" style="grid-column-start: ${primerDia};"> <time datetime="">${i+1}</div>`;
+                if(primerDia === 0){
+                    diasHTML += `<div class="primerDia" style="grid-column-start: ${7};"> <time datetime="">${i+1}</div>`;
+                }else{
+                    diasHTML += `<div class="primerDia" style="grid-column-start: ${primerDia};"> <time datetime="">${i+1}</div>`;
+                }
             }else{
                 diasHTML += `<div>${i+1}</div>`;
             }
         }
         return diasHTML;
     }
-}
-
-
-
-
-
-
-function conseguirDiaActual(){
-    today = new Date();
-    
-    pintarAnio(today.getFullYear());
-}
-
-function pintarAnio(anio){
-    nodoAnio.innerText = anio;
 }

9675cd91b8549fa8069736c26a5f2e475f9224f7
Pablo <pgilp17@protonmail.com>
2022-11-02 11:54:09 +0100

    Tres en raya

--- Clase18/TresEnRaya/app.js
+++ Clase18/TresEnRaya/app.js
@@ -1,8 +1,14 @@
 'use strict'
 //Elegir nombres de jugadores y tipos de juego (Implementar después de tener el juego)
-let player1   = 'Jugador 1';
-let player2   = 'Jugador 2';
-let tipoJuego = 3;
+let player1       = 'Jugador 1';
+let player2       = 'Jugador 2';
+let tipoJuego     = 3;
+let turno         = 0;
+let estadoTablero = [
+    [ '' , '' , '' ] ,
+    [ '' , '' , '' ] ,
+    [ '' , '' , '' ]
+];
 
 //Juego
 
@@ -13,7 +19,6 @@ filtraJuego(player1,player2,tipoJuego);
 
 function filtraJuego(player1,player2,tipoJuego){
 
-    generarTablero(tipoJuego);
     switch (tipoJuego) {
         case 3:
             nodoTablero.classList.add('tresRaya');
@@ -35,28 +40,67 @@ function filtraJuego(player1,player2,tipoJuego){
             empiezaTresEnRaya(player1,player2);
             break;
     }
+    generarTablero(tipoJuego);
 }
 
 //Generacion tablero
-function generarTablero(tipoJuego){
-    console.log(tipoJuego);
 
-    for (let i = 0; i < Math.pow(tipoJuego,2); i++) {
-        let newDiv = document.createElement('div');
-        let newImg = document.createElement('img');
-        newDiv.appendChild(newImg);
-        newDiv.addEventListener('click', function(){
-            pintaCirculo(this.firstChild);
-        });
-        nodoTablero.appendChild(newDiv);
-        
+function generarTablero(tipoJuego) {
+    for (let c = 0; c < tipoJuego; c++) {
+        console.log('hola');
+        generarFilas(c);
     }
-
+    
 }
 
-function pintaCirculo(img) {
-    let nodoImg = new HTMLImageElement(img);
-    nodoImg.setAtribbute('src', './svg/circle.svg');
+function generarFilas(c){
+
+    for (let f = 0; f < tipoJuego; f++) {
+        let divPosicion = document.createElement('div');
+        divPosicion.id = String(c)+String(f);
+        let img = document.createElement('img');
+        divPosicion.appendChild(img);
+        //Estoy desarrollando no poder sobreponer equis y circulos
+        divPosicion.addEventListener('click', function(evento){
+            console.log();
+            if(this.firstChild.innerHTML === ''){
+
+                pinta(this.firstChild);
+                actualizaEstadoTablero(this.id);
+            }
+            // checkWin();
+        });
+        nodoTablero.appendChild(divPosicion);
+    }
+    
+}
+
+
+function actualizaEstadoTablero(posicion){
+    let fila = posicion.charAt(0);
+    let columna = posicion.charAt(1);
+
+    estadoTablero[fila][columna] = (turno === 0) ? 'O' : 'X';
+    if(turno === 0){
+        console.log('primerTurno');
+        estadoTablero[fila][columna] = 'O';
+        console.log(estadoTablero);
+        turno++;
+    }else if(turno === 1){
+        console.log('segundoTurno');
+        estadoTablero[fila][columna] = 'X';
+        console.log(estadoTablero);
+        turno--;
+    }
+};
+
+
+function pinta(img) {
+    if(turno === 0){
+        img.src =  './svg/circle.svg';
+    }else if(turno === 1){
+        img.src =  './svg/cross.svg';
+    }
 }
 
 
@@ -79,11 +123,7 @@ function pintaCirculo(img) {
 function empiezaTresEnRaya(player1,player2){
     console.log('hola');
     let win = true;
-    let estadoTablero = [
-        [ '' , '' , '' ] ,
-        [ '' , '' , '' ] ,
-        [ '' , '' , '' ]
-    ];
+
 
     // do {
         
@@ -96,7 +136,21 @@ function empiezaTresEnRaya(player1,player2){
 
 }
 
-function checkWin(tableroJuego){
+function checkWin(){
     
     return false;
+}
+
+
+function resetearJuego(){
+    nodoTablero.innerHTML = '';
+
+    //Preguntar tipo de juego y volver a iniciar;
+}
+
+function reiniciarJuego(){
+    nodoTablero.innerHTML = '';
+    filtraJuego(player1,player2,tipoJuego);
+
+    //Reiniciar el mismo tipo de juego con los mismos jugadores;
 }

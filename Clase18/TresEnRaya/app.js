c81d5666d96c5ef1fa4b565e108eaff3c24a9630
Pablo <pgilp17@protonmail.com>
2022-11-03 11:57:28 +0100

    TresRaya

--- Clase18/TresEnRaya/app.js
+++ Clase18/TresEnRaya/app.js
@@ -1,156 +1,191 @@
 'use strict'
 //Elegir nombres de jugadores y tipos de juego (Implementar después de tener el juego)
-let player1       = 'Jugador 1';
-let player2       = 'Jugador 2';
-let tipoJuego     = 3;
-let turno         = 0;
-let estadoTablero = [
-    [ '' , '' , '' ] ,
-    [ '' , '' , '' ] ,
-    [ '' , '' , '' ]
-];
+let eventoBoton = function(){
+    let resultado = false;
+    if(this.firstChild.src === ''){
+        pinta(this.firstChild);
+        actualizaEstadoTablero(this.id);
+        resultado = checkWin();
+        if(resultado === true){
+            pintaVictoria();
+        }
+    }
+}
 
-//Juego
 
-//Puede haber distintos tipos de juego --> 3*3, 4*4, 7*7
+let changeClassPlayer = document.querySelectorAll('.color');
+let nodoPlayerMoving  = document.querySelector('.playerMoving');
+let player1           = 'Jugador 1';
+let player2           = 'Jugador 2';
+let tipoJuego         = 3;
+let turno             = 0; // 0 --> Primer jugador  ^^^^^ 1 --> Segundo jugador
+let estadoTablero     = [
+                          [ '' , '' , '' ] ,
+                          [ '' , '' , '' ] ,
+                          [ '' , '' , '' ]
+                        ];
+
 let nodoTablero = document.querySelector('.tablero');
 
-filtraJuego(player1,player2,tipoJuego);
+filtraJuego(tipoJuego);
+generarTablero(tipoJuego);
 
-function filtraJuego(player1,player2,tipoJuego){
+function filtraJuego(tipoJuego){
+    nodoPlayerMoving.innerHTML = `<h1>Juega ${player1}</h1>`;
 
     switch (tipoJuego) {
         case 3:
             nodoTablero.classList.add('tresRaya');
-            empiezaTresEnRaya(player1,player2);
             break;
         
         case 4:
             nodoTablero.classList.add('cuatroRaya');
-            empiezaCuatroEnRaya(player1,player2);
             break;
     
         case 7:
             nodoTablero.classList.add('sieteRaya');
-            empiezaSieteEnRaya(player1,player2);
             break;
 
         default:
             nodoTablero.classList.add('tresRaya');
-            empiezaTresEnRaya(player1,player2);
             break;
     }
-    generarTablero(tipoJuego);
+    
 }
 
 //Generacion tablero
 
 function generarTablero(tipoJuego) {
+    generarColumnas(tipoJuego);
+}
+function generarColumnas(){
     for (let c = 0; c < tipoJuego; c++) {
-        console.log('hola');
         generarFilas(c);
     }
-    
 }
 
-function generarFilas(c){
 
+function generarFilas(c){
     for (let f = 0; f < tipoJuego; f++) {
         let divPosicion = document.createElement('div');
+        divPosicion.classList.add('posicion');
         divPosicion.id = String(c)+String(f);
         let img = document.createElement('img');
         divPosicion.appendChild(img);
-        //Estoy desarrollando no poder sobreponer equis y circulos
-        divPosicion.addEventListener('click', function(evento){
-            console.log();
-            if(this.firstChild.innerHTML === ''){
-
-                pinta(this.firstChild);
-                actualizaEstadoTablero(this.id);
-            }
-            // checkWin();
-        });
+        divPosicion.addEventListener('click', eventoBoton);
         nodoTablero.appendChild(divPosicion);
     }
-    
 }
 
+
+function pintaVictoria(){
+    let listPosicion = document.querySelectorAll('.posicion');
+    listPosicion.forEach(element => {
+        element.removeEventListener('click',eventoBoton);
+    })
+    nodoPlayerMoving.innerHTML = '<h1>VICTORIAAAA</h1>';
+}
 
 function actualizaEstadoTablero(posicion){
     let fila = posicion.charAt(0);
     let columna = posicion.charAt(1);
-
-    estadoTablero[fila][columna] = (turno === 0) ? 'O' : 'X';
+    
     if(turno === 0){
         console.log('primerTurno');
         estadoTablero[fila][columna] = 'O';
-        console.log(estadoTablero);
         turno++;
+        nodoPlayerMoving.innerHTML = `<h1>Juega ${player2}</h1>`;
+        changeClassPlayer.forEach(element => {
+            element.classList.remove('player1');
+            element.classList.add('player2');
+        });
     }else if(turno === 1){
         console.log('segundoTurno');
         estadoTablero[fila][columna] = 'X';
-        console.log(estadoTablero);
         turno--;
+        nodoPlayerMoving.innerHTML = `<h1>Juega ${player1}</h1>`;
+        changeClassPlayer.forEach(element => {
+            element.classList.remove('player2');
+            element.classList.add('player1');
+        });
     }
 };
 
-
 function pinta(img) {
     if(turno === 0){
-        img.src =  './svg/circle.svg';
+        img.src = './svg/circle.svg';
     }else if(turno === 1){
-        img.src =  './svg/cross.svg';
+        img.src = './svg/cross.svg';
     }
 }
 
+function checkWin(){
+    let result = false;
+    if(result === false){
+        result = comprobarDerecha(estadoTablero) || comprobarAbajo(estadoTablero) || comprobarDiagonal(estadoTablero);
+        console.log(result);
+    }
+    return result;
+    }
+    
+function comprobarDerecha(arr){
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
 
-//Empieza jugador 1 (circulos), pincha en un div y se pinta un circulo
-//Cambio de jugador y por tanto de color
-//Jugador 2, pincha en un div y se pinta una raya
-// .
-// .
-// .
-// .
-// .
-// Si alguno consigue 3 en raya gana
-// .
-// .
-// .
-// .
-// Se llena el tablero y nadie tiene tres en raya, entonces empate
+            let comprueba = arr[i][j]   !== '' &&
+                            arr[i][j]   === arr[i][j+1] && 
+                            arr[i][j+1] === arr[i][j+2];
 
-
-function empiezaTresEnRaya(player1,player2){
-    console.log('hola');
-    let win = true;
-
-
-    // do {
-        
-        
-    //     win = checkWin(estadoTablero);
-
-    // } while (!win);
-
-    //Elegir jugador ganador
-
+            if(comprueba){
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
-function checkWin(){
-    
+function comprobarAbajo(arr){
+        
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < (arr[i].length-2); j++) {
+
+            let comprueba = arr[i][j]   !== '' &&
+                            arr[i][j]   === arr[i+1][j] && 
+                            arr[i+1][j] === arr[i+2][j];
+
+            if(comprueba){
+            return true;
+            }
+        }
+    }
     return false;
 }
 
 
+function comprobarDiagonal(arr){
+        
+    for (let i = 0; i < (arr.length-2); i++) {
+        console.log('primerArray-->',arr[i].length);
+        for (let j = 0; j < (arr[i].length-2); j++) {
+            
+            let comprueba = arr[i][j]     !== '' &&
+                            arr[i][j]     === arr[i+1][j+1] && 
+                            arr[i+1][j+1] === arr[i+2][j+2] 
+
+            if(comprueba){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 function resetearJuego(){
     nodoTablero.innerHTML = '';
-
-    //Preguntar tipo de juego y volver a iniciar;
 }
 
 function reiniciarJuego(){
     nodoTablero.innerHTML = '';
-    filtraJuego(player1,player2,tipoJuego);
-
-    //Reiniciar el mismo tipo de juego con los mismos jugadores;
+    filtraJuego(tipoJuego);
+    generarTablero(tipoJuego);
 }

f05b39891ff90f490dcb438d583770fd49922876
Pablo <pgilp17@protonmail.com>
2022-11-14 12:01:49 +0100

    Cronometro

--- Clase25/Cronometro/app.js
+++ Clase25/Cronometro/app.js
@@ -17,32 +17,15 @@ let nodoCentesimas = document.querySelector('.centesimas');
 
 let buttonPause     = document.querySelector( '#pause');
 let buttonStart     = document.querySelector( '#start');
-let buttonContinuar = document.querySelector( '#continue');
-
-
-
-
+let buttonContinue = document.querySelector( '#continue');
 
 buttonStart.addEventListener('click',function(){
     if(!buttonStart.classList.contains('disabled')){
         let empiezaCrono = new Date().getTime();
         console.log(String(new Date().getTime() - empiezaCrono));
         console.log(empiezaCrono);
-        interval = setInterval(function(){
-            
-            diferencia = (new Date().getTime() - empiezaCrono); 
-            centesima  = Math.trunc((diferencia/10)%100);
-            segundo    = Math.trunc((diferencia/1000)%60);
-            minuto     = Math.trunc((diferencia/60000)%60);
-            hora       = Math.trunc((diferencia/(60000*60))%60);
-            
-            
-            nodoCentesimas.innerHTML = centesima;
-            nodoSegundos.innerHTML   = segundo;
-            nodoMinutos.innerHTML    = minuto;
-            nodoHoras.innerHTML      = hora;
+        iniciarIntervalo(0,empiezaCrono);
 
-        },10);
         buttonPause.classList.remove('disabled');
         buttonStart.classList.add('disabled');
     }
@@ -51,40 +34,47 @@ buttonStart.addEventListener('click',function(){
 buttonPause.addEventListener('click',function(){
     if(!buttonPause.classList.contains('disabled')){
         pause();
+        buttonContinue.classList.remove('disabled');
     }
 });
 
-buttonContinuar.addEventListener('click',function(){
-    continueCrono();
+buttonContinue.addEventListener('click',function(){
+    if(!buttonContinue.classList.contains('disabled')){
+        
+        continueCrono();
+        buttonContinue.classList.add('disabled');
+    }
 });
 
 function pause(){
     clearInterval(interval);
-    let empiezaCrono = new Date().getTime();
-    console.log(String(new Date().getTime() - empiezaCrono));
-    console.log(empiezaCrono);
-    console.log(centesima,segundo,minuto,hora);    
 }
 
 function continueCrono(){
-    let empiezaCrono = new Date().getTime()+centesima;
-    console.log(centesima,segundo,minuto,hora);
-    console.log(String(new Date().getTime() - empiezaCrono));
-    console.log(empiezaCrono);
-    console.log(empiezaCrono+centesima);
+
+    let tiempoTranscurrido = (hora*60*60*1000)+(minuto*60*100)+(segundo*1000)+(centesima*10);  
+    let empiezaCrono = new Date().getTime();
+    console.log('Momento actual al empezar el crono-->',new Date().getTime());
+    
+    
+    clearInterval(interval);
+    iniciarIntervalo(tiempoTranscurrido,empiezaCrono);
+}
+
+function iniciarIntervalo(tiempoTranscurrido,empiezaCrono){
     interval = setInterval(function(){
-            
-        diferencia = (new Date().getTime() - empiezaCrono); 
+        
+        diferencia = ((new Date().getTime()+tiempoTranscurrido) - empiezaCrono);
+
         centesima  = Math.trunc((diferencia/10)%100);
         segundo    = Math.trunc((diferencia/1000)%60);
         minuto     = Math.trunc((diferencia/60000)%60);
         hora       = Math.trunc((diferencia/(60000*60))%60);
         
-        
-        nodoCentesimas.innerHTML = centesima;
-        nodoSegundos.innerHTML   = segundo;
-        nodoMinutos.innerHTML    = minuto;
-        nodoHoras.innerHTML      = hora;
+        nodoCentesimas.innerHTML = (centesima < 10) ? '0' + centesima : centesima;
+        nodoSegundos.innerHTML   = (segundo < 10)   ? '0' + segundo : segundo;
+        nodoMinutos.innerHTML    = (minuto < 10)    ? '0' + minuto : minuto;
+        nodoHoras.innerHTML      = (hora < 10)      ? '0' + hora : hora;
 
     },10);
 }

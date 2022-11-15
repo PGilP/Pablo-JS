8896495afb485d9e96073d173e12c6967c74699c
Pablo <pgilp17@protonmail.com>
2022-11-15 11:58:38 +0100

    Mejora Sorteo

--- Clase28/Sorteo/app.js
+++ Clase28/Sorteo/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 document.querySelector('.add').addEventListener('click',function(){
-    if(document.querySelector('#nombre').value){
+    if(document.querySelector('#nombre').value && !document.querySelector('.add').classList.contains('disabled')){
         const newDiv = document.createElement('div');
         newDiv.classList.add('person');
         newDiv.innerText = document.querySelector('#nombre').value;
@@ -10,31 +10,38 @@ document.querySelector('.add').addEventListener('click',function(){
 });
 
 document.querySelector('.sortea').addEventListener('click',function(){
-    let nodoParticipantes = document.querySelectorAll('.person');
-    let numParticipantes  = nodoParticipantes.length;
-    
-    for (let i = 0; i < nodoParticipantes.length; i++) {
-        nodoParticipantes[i].classList.remove('winner');    
-    }
-    let contador = 0; 
-    let interval = setInterval(() => {
+    if(!document.querySelector('.add').classList.contains('disabled')){
+
+        let nodoParticipantes = document.querySelectorAll('.person');
+        let numParticipantes  = nodoParticipantes.length;
         
         for (let i = 0; i < nodoParticipantes.length; i++) {
-            nodoParticipantes[i].classList.remove('waiting');    
+            nodoParticipantes[i].classList.remove('winner');    
         }
-        if(contador > (nodoParticipantes.length)-1){
-            contador = 0;
-        }
-        nodoParticipantes[contador++].classList.add('waiting');
-    }, 100);
-
-    let timeout = setTimeout(() => {
-        for (let i = 0; i < nodoParticipantes.length; i++) {
-            nodoParticipantes[i].classList.remove('waiting');    
-        }
-        nodoParticipantes[getRandomInt(numParticipantes)].classList.add('winner');
-        clearInterval(interval);
-    }, 2000);
+        let contador = 0; 
+        let interval = setInterval(() => {
+            document.querySelector('.sortea').classList.add('disabled');
+            document.querySelector('.add').classList.add('disabled');
+            for (let i = 0; i < nodoParticipantes.length; i++) {
+                nodoParticipantes[i].classList.remove('waiting');    
+            }
+            if(contador > (nodoParticipantes.length)-1){
+                contador = 0;
+            }
+            nodoParticipantes[contador++].classList.add('waiting');
+        }, 100);
+    
+        let timeout = setTimeout(() => {
+    
+            for (let i = 0; i < nodoParticipantes.length; i++) {
+                nodoParticipantes[i].classList.remove('waiting');    
+            }
+            nodoParticipantes[getRandomInt(numParticipantes)].classList.add('winner');
+            clearInterval(interval);
+            document.querySelector('.sortea').classList.remove('disabled');
+            document.querySelector('.add').classList.remove('disabled');
+        }, 2000);
+    }
 
 
 });

91a1cd0a95732237d2e1250629248eed94b41ca7
Pablo <pgilp17@protonmail.com>
2022-11-22 11:56:44 +0100

    EmpiezoTablaDinamica

--- Clase30/ContadorLetras/app.js
+++ Clase30/ContadorLetras/app.js
@@ -1,18 +1,47 @@
 'use strict'
 
-let arrLetras = new Array();
+// let arrLetras = new Array();
 
-document.querySelector('#contador').addEventListener('input',function(ev){
-    let valor = ev.data;
-    if(valor === null){
-        arrLetras.pop();
-    }else{
-        arrLetras.push(valor);
-    }
-    for (let i = 0; i < arrLetras.length; i++) {
-        const element = array[i];
-        if(){}
+// let nodoResult = document.querySelector('.result');
+
+// document.querySelector('#contador').addEventListener('input',function(ev){
+//     let valor = ev.data;
+//     if(valor === null){
+//         arrLetras.pop();
+//     }else if( valor === /[a-zA-Z]/){
+//         arrLetras.push(valor);
+//     }
+
+//     let arrLetrasOrdenado = [].concat(arrLetras);
+//     arrLetrasOrdenado.sort();
+//     console.log(arrLetrasOrdenado);
+    
+    
+    
+//     for (let i = 0; i < arrLetrasOrdenado.length; i++) {
         
-    }
-    console.log(arrLetras);
-});
+//         const element = arrLetrasOrdenado[i];
+//         let cont = 0;
+//         do{
+//             console.log(element);
+//             cont++;
+//         }while(element !== arrLetrasOrdenado[i+1]);
+//         console.log(cont);
+//     }
+    
+    
+// });
+
+let json_object = `
+{
+    "nombre" : "Titanic",
+    "director" : "James Cameron"
+}`;
+
+let object = JSON.parse(json_object);
+
+console.log(object);
+
+
+let object_json = JSON.stringify(object);
+console.log(object_json);

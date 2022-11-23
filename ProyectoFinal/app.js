a6098f7450b3d0cc107412585039a88c7ef42344
Pablo <pgilp17@protonmail.com>
2022-11-23 12:00:26 +0100

    MaquetacionProyecto

---
+++ ProyectoFinal/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+//Posicionamiento
+let nodoHeader = document.querySelector('.header');
+console.log(nodoHeader);
+
+let tasks = {
+    toDo:[
+        {
+            name: '',
+            state: '',
+        }
+    ],
+    doing:[],
+    complete:[]
+}

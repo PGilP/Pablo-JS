35d2c42a80ab35f95d0eebee251736b3317a47f5
Pablo <pgilp17@protonmail.com>
2022-11-29 11:56:37 +0100

    ContinuamosProyecto

--- ProyectoFinal/app.js
+++ ProyectoFinal/app.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 //Metodo para obtener un hash para cada tarea
 String.prototype.hashCode = function() {
     var hash = 0,
@@ -14,60 +13,41 @@ String.prototype.hashCode = function() {
     return hash;
 }
 
-
 let tasks = {
     toDo:[],
     doing:[],
     complete:[]
 }
+
 let functionCheck = function(){
-    console.log(this.parentNode.parentNode);
-    let nameTask = this.parentNode.parentNode.querySelector('.name').innerText;
+    let task = this.closest('.task');
+    let nameTask = task.querySelector('.name').innerText;
+
     if(tasks.toDo.find(element => element.name === nameTask)){
-        let elementToDo = tasks.toDo.find(element => element.name === nameTask);
-        console.log(elementToDo);
-        tasks.doing.push(elementToDo);
-        
-        tasks.toDo = tasks.toDo.filter((item) => item.name !== nameTask);
-        console.log(tasks);
-        visualDelete(this.parentNode.parentNode);
-
-        //Generamos el elemento que pasa al doing
-        visualTratementTask('doing',elementToDo.name);
-        
+        filterTasks('toDo','doing');        
     }else if(tasks.doing.find(element => element.name === nameTask)){
-
-        let elementDoing = tasks.doing.find(element => element.name === nameTask);
-        console.log(elementDoing);
-        tasks.complete.push(elementDoing);
-        
-        tasks.doing = tasks.doing.filter((item) => item.name !== nameTask);
-        console.log(tasks);
-        visualDelete(this.parentNode.parentNode);
-
-        //Generamos el elemento que pasa al doing
-        visualTratementTask('complete',elementDoing.name);
-
+        filterTasks('doing','complete');
     }else if(tasks.complete.find(element => element.name === nameTask)){
+        filterTasks('complete','doing');
+    }
 
-        let elementComplete = tasks.complete.find(element => element.name === nameTask);
-        console.log(elementComplete);
-        tasks.doing.push(elementComplete);
+    function filterTasks(arrFind,arrAdd){
+
+        let element = tasks[arrFind].find(element => element.name === nameTask);
+        tasks[arrAdd].push(element);
         
-        tasks.complete = tasks.complete.filter((item) => item.name !== nameTask);
-        console.log(tasks);
-        visualDelete(this.parentNode.parentNode);
+        tasks[arrFind] = tasks[arrFind].filter((item) => item.name !== nameTask);
+        visualDelete(task);
 
-        //Generamos el elemento que pasa al doing
-        visualTratementTask('doing',elementComplete.name);
+        visualTratementTask(arrAdd,element.name);
 
     }
 
     localStorage.setItem('tasks',JSON.stringify(tasks));
 }
 let functionDelete = function(){
-    console.log(this.parentNode.parentNode);
-    let nameTask = this.parentNode.parentNode.querySelector('.name').innerText;
+    let task = this.closest('.task');
+    let nameTask = task.querySelector('.name').innerText;
     if(tasks.toDo.find(element => element.name === nameTask)){
         tasks.toDo = tasks.toDo.filter((item) => item.name !== nameTask);
     }else if(tasks.doing.find(element => element.name === nameTask)){
@@ -77,7 +57,7 @@ let functionDelete = function(){
     }
     console.log(tasks);
     localStorage.setItem('tasks',JSON.stringify(tasks));
-    visualDelete(this.parentNode.parentNode);
+    visualDelete(task);
 }
 
 let functionCreateTask = function(){
@@ -102,24 +82,6 @@ let functionCreateTask = function(){
     }
 }
 
-if(localStorage.getItem('tasks')){
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-    tasks.toDo.forEach(element => {
-        console.log(element.name);
-        visualTratementTask('toDo',element.name);
-    });
-
-    tasks.doing.forEach(element => {
-        console.log(element.name);
-        visualTratementTask('doing',element.name);
-    });
-
-    tasks.complete.forEach(element => {
-        console.log(element.name);
-        visualTratementTask('complete',element.name);
-    });
-}
-
 document.querySelector('#taskName').addEventListener('input',function(){
     document.querySelector('#taskName').classList.remove('empty');
     document.querySelector('#taskName').placeholder = 'Introduce una tarea';
@@ -128,86 +90,85 @@ document.querySelector('#taskName').addEventListener('input',function(){
 
 document.querySelector('#addTask').addEventListener('click',functionCreateTask);
 
-//No es aqui
-document.querySelector('#addTask').addEventListener('keypress',functionCreateTask);
+document.querySelector('#taskName').addEventListener('keypress',function(ev){
+    if(ev.key === 'Enter'){
+        functionCreateTask();
+    }
+});
 
+if(localStorage.getItem('tasks')){
+    tasks = JSON.parse(localStorage.getItem('tasks'));
 
+    tasks.toDo.forEach(element => {
+        visualTratementTask('toDo',element.name);
+    });
 
-//Crear TAREA
+    tasks.doing.forEach(element => {
+        visualTratementTask('doing',element.name);
+    });
+
+    tasks.complete.forEach(element => {
+        visualTratementTask('complete',element.name);
+    });
+}
+
 
 function visualTratementTask(state,taskName){
     let elementTask = document.createElement('div');
     elementTask.classList.add('task', state);
-    //Falta crear data-reference
-        let elementName = document.createElement('div');
-        elementName.classList.add('name');
-        elementName.innerText = taskName;
-        elementTask.append(elementName);
+    let elementName = document.createElement('div');
+    elementName.classList.add('name');
+    elementName.innerText = taskName;
+    elementTask.append(elementName);
+
+    let elementActions = document.createElement('div');
+    elementActions.classList.add('actions');
     
-        let elementActions = document.createElement('div');
-        elementActions.classList.add('actions');
+    //Este elemento es el unico que va a cambiar al pasar a doing
+    if(state === 'toDo'){
+        createCheck('Empieza la tarea','arrow_downward');
+    }else if( state === 'doing' || state === 'complete' ){
+        createCheck('Completa la tarea','check_circle');
+    }
+
+    let elementDelete = document.createElement('div');
+    elementDelete.classList.add('delete');
+    elementDelete.addEventListener('click',functionDelete);
         
-            //Este elemento es el unico que va a cambiar al pasar a doing
-            if(state === 'toDo'){
-                let elementCheck = document.createElement('div');
-                elementCheck.classList.add('check');
-                elementCheck.addEventListener('click',functionCheck);
-                let elementSpanCheck = document.createElement('span');
-                elementSpanCheck.classList.add('material-symbols-outlined');
-                elementSpanCheck.innerText = 'arrow_downward';
-                let elementTooltipCheck = document.createElement('div');
-                elementTooltipCheck.classList.add('tooltipText');
-                elementTooltipCheck.innerHTML = 'Empieza la tarea';
-                
-                elementCheck.append(elementSpanCheck);
-                elementCheck.append(elementTooltipCheck);
-
-                elementActions.append(elementCheck);
-
-            }else if( state === 'doing' || state === 'complete' ){
-
-                let elementCheck = document.createElement('div');
-                elementCheck.classList.add('check');
-                elementCheck.addEventListener('click',functionCheck);
-                let elementSpanCheck = document.createElement('span');
-                elementSpanCheck.classList.add('material-symbols-outlined');
-                elementSpanCheck.innerText = 'check_circle';
-                let elementTooltipCheck = document.createElement('div');
-                elementTooltipCheck.classList.add('tooltipText');
-                elementTooltipCheck.innerHTML = 'Completa la tarea';
-                
-                elementCheck.append(elementSpanCheck);
-                elementCheck.append(elementTooltipCheck);
-
-                elementActions.append(elementCheck);
-
-            }
-            
-            //Fin del elemento que cambia
+    let elementSpanDelete = document.createElement('span');
+    elementSpanDelete.classList.add('material-symbols-outlined');
+    elementSpanDelete.innerText = 'delete';
+    let elementTooltipDelete = document.createElement('div');
+    elementTooltipDelete.classList.add('tooltipText');
+    elementTooltipDelete.innerHTML = 'Elimina la tarea';
     
-            let elementDelete = document.createElement('div');
-            elementDelete.classList.add('delete');
-            elementDelete.addEventListener('click',functionDelete);
-                
-                let elementSpanDelete = document.createElement('span');
-                elementSpanDelete.classList.add('material-symbols-outlined');
-                elementSpanDelete.innerText = 'delete';
-                let elementTooltipDelete = document.createElement('div');
-                elementTooltipDelete.classList.add('tooltipText');
-                elementTooltipDelete.innerHTML = 'Elimina la tarea';
-            
-            elementDelete.append(elementSpanDelete);
-            elementDelete.append(elementTooltipDelete);
-        elementActions.append(elementDelete);
+    elementDelete.append(elementSpanDelete);
+    elementDelete.append(elementTooltipDelete);
+    elementActions.append(elementDelete);
     
     elementTask.append(elementActions);
     
     document.querySelector(`.${state}Section .listTasks`).append(elementTask);
     
+    function createCheck(textTooltip,iconSpan){
+
+        let elementCheck = document.createElement('div');
+        elementCheck.classList.add('check');
+        elementCheck.addEventListener('click',functionCheck);
+        let elementSpanCheck = document.createElement('span');
+        elementSpanCheck.classList.add('material-symbols-outlined');
+        elementSpanCheck.innerText = iconSpan;
+        let elementTooltipCheck = document.createElement('div');
+        elementTooltipCheck.classList.add('tooltipText');
+        elementTooltipCheck.innerHTML = textTooltip;
+        
+        elementCheck.append(elementSpanCheck);
+        elementCheck.append(elementTooltipCheck);
+
+        elementActions.append(elementCheck);
+
+    }
 }
-
-
-//Eliminar visualmente el elemento
 
 function visualDelete(element){
     element.remove();

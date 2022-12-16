dacfd478ea0996c94476c07d12109b5feffc16ea
Pablo <pgilp17@protonmail.com>
2022-12-16 11:56:22 +0100

    Angulaaaaaar

--- angular/WebRutas/src/app/components/to-do-list/to-do-list.component.ts
+++ angular/WebRutas/src/app/components/to-do-list/to-do-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskList } from 'src/app/models/tasks.model';
+import { Task } from 'src/app/models/tasks.model';
 
 @Component({
   selector: 'app-to-do-list',
@@ -21,11 +22,23 @@ export class ToDoListComponent implements OnInit {
   }
 
   addTask(){
-    let newTask = {
-      name: this.inputText
+    if(this.inputText){
+      
+      let newTask:Task = {
+        name : this.inputText
+      }
+
+      this.tasks.toDo.push(newTask);
+
     }
-    this.tasks.toDo.push(newTask);
     this.inputText = '';
+  }
+
+  public startTask(task:Task){
+    console.log(task);
+  }
+  public deleteTask(task:Task){
+    console.log(task);
   }
 
 }
